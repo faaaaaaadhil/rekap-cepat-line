889a5dabb79b19fe2d46f4b5fc38d991fde2ac3e
Firmansyah Nuralif Rohman <firmanrpl16@gmail.com>
2017-10-06 20:29:59 +0700

    YEAH

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const drx = require('dropbox');
 
-const dropbox = new drx('6N6VX7AUflAAAAAAAAAAFNYtTdxYjRpGQrVoRVwqJlP7pXxOJNJguBWeVCSA-CYQ');
+const dropbox = new drx({ accessToken: '6N6VX7AUflAAAAAAAAAAFNYtTdxYjRpGQrVoRVwqJlP7pXxOJNJguBWeVCSA-CYQ' });
 const config = {
     channelAccessToken: "m6oMTBwJCVIChmaV5Ek7uCVaq6CTNmm71Dg/OwJoOHFl59GAXah/4gaz5QFYaYrRtNrU8iucgzu1FIYlH2k5kTI1L+sug8gkVUZ+NO8Ll4qmICGSQKBidrAzK2q424lv5Jp/aGnmtXiOmSLtGn/IjQdB04t89/1O/w1cDnyilFU=",
     channelSecret: "03964adaec21355bf3f78f058079ab03",
