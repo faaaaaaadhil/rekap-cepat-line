724094204301a74ce61e33aaef9b0208fb22fb09
Firmansyah Nuralif Rohman <firmanrpl16@gmail.com>
2017-10-06 19:37:47 +0700

    YEAH

--- app.js
+++ app.js
@@ -4,17 +4,14 @@ const line = require('@line/bot-sdk');
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-const admin = require("firebase-admin");
-const service = require('./serviceAccountKey.json');
+const mongoose = require('mongoose');
+const google = require('googleapis');
+const googleAuth = require('google-auth-library');
 
-admin.initializeApp({
-    credential: admin.credential.cert(service),
-    databaseURL: "https://firmanbotline.firebaseio.com"
-});
-
-const db = admin.database();
-const auth = admin.auth();
-const ref = db.ref("server/saving-data/fireblog");
+var SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
+var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+    process.env.USERPROFILE) + '/.credentials/';
+var TOKEN_PATH = TOKEN_DIR + 'drive-nodejs-quickstart.json';
 
 const config = {
     channelAccessToken: "m6oMTBwJCVIChmaV5Ek7uCVaq6CTNmm71Dg/OwJoOHFl59GAXah/4gaz5QFYaYrRtNrU8iucgzu1FIYlH2k5kTI1L+sug8gkVUZ+NO8Ll4qmICGSQKBidrAzK2q424lv5Jp/aGnmtXiOmSLtGn/IjQdB04t89/1O/w1cDnyilFU=",
@@ -35,19 +32,7 @@ app.post('/webhook', line.middleware(config), function(req, res){
 });
 
 function handleEvent(event){
-    const userRef = ref.child("users");
-    userRef.set({
-        alanisawesome: {
-          date_of_birth: "June 23, 1912",
-          full_name: "Alan Turing"
-        },
-        gracehop: {
-          date_of_birth: "December 9, 1906",
-          full_name: "Grace Hopper"
-        }
-      });
-    const echo = { type: 'sticker', stickerId: event.message.stickerId, packageId: event.message.packageId };
-    return clientLine.replyMessage(event.replyToken, echo);
+
 }
 
 const port = process.env.PORT || 3000;
