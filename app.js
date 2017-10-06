c2e0ea36d318107ba7a0e6e9819f4429c0b801be
Firmansyah Nuralif Rohman <firmanrpl16@gmail.com>
2017-10-06 20:24:45 +0700

    YEAH

--- app.js
+++ app.js
@@ -5,14 +5,9 @@ const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const google = require('googleapis');
-const googleAuth = require('google-auth-library');
+const drx = require('dropbox');
 
-var SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
-var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
-    process.env.USERPROFILE) + '/.credentials/';
-var TOKEN_PATH = TOKEN_DIR + 'drive-nodejs-quickstart.json';
-
+const dropbox = new drx('6N6VX7AUflAAAAAAAAAAFNYtTdxYjRpGQrVoRVwqJlP7pXxOJNJguBWeVCSA-CYQ');
 const config = {
     channelAccessToken: "m6oMTBwJCVIChmaV5Ek7uCVaq6CTNmm71Dg/OwJoOHFl59GAXah/4gaz5QFYaYrRtNrU8iucgzu1FIYlH2k5kTI1L+sug8gkVUZ+NO8Ll4qmICGSQKBidrAzK2q424lv5Jp/aGnmtXiOmSLtGn/IjQdB04t89/1O/w1cDnyilFU=",
     channelSecret: "03964adaec21355bf3f78f058079ab03",
@@ -32,7 +27,7 @@ app.post('/webhook', line.middleware(config), function(req, res){
 });
 
 function handleEvent(event){
-
+    console.log('Dropbox Status : ' + dropbox.usersGetAccount.name);
 }
 
 const port = process.env.PORT || 3000;
